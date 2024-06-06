a36da627e93fb788384e90c7b5e3edb522727508
catwifhood <165341190+catwifhood@users.noreply.github.com>
2024-06-06 19:00:10 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header() {
   return (
     <div className="px-20 py-10 flex md:flex-row flex-col justify-between items-center gap-10 text-white">
-      <h1 className="text-2xl font-bold">CA: COMING SOON</h1>
+      <h1 className="text-2xl font-bold">CA: HEyYUg7zW89buR4bryY4EDDsCQzsqDJW62GBPSampump</h1>
       <div className="flex-grow md:block hidden" />
       <div className="flex gap-5 justify-center items-center rounded-lg bg-white/30 p-5 py-2 border border-white">
         <a href="https://t.me/chonkesolana" target="_blank" rel="noreferrer">
@@ -12,7 +12,7 @@ export default function Header() {
         <a href="https://x.com/ChonkeOnSolana" target="_blank" rel="noreferrer">
           <img src="/x.png" alt="image" className="w-10 h-10" />
         </a>
-        <a href="https://pump.fun/" target="_blank" rel="noreferrer">
+        <a href="https://pump.fun/HEyYUg7zW89buR4bryY4EDDsCQzsqDJW62GBPSampump" target="_blank" rel="noreferrer">
           <img src="/pill.png" alt="image" className="w-10 h-10" />
         </a>
       </div>

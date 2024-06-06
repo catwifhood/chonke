7d24978d99326b1988e9e39a2cc33b3e10cf3bb7
catwifhood <165341190+catwifhood@users.noreply.github.com>
2024-06-06 19:02:31 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,6 @@ import React from "react";
 export default function Header() {
   return (
     <div className="px-20 py-10 flex md:flex-row flex-col justify-between items-center gap-10 text-white">
-      <h1 className="text-2xl font-bold">CA: HEyYUg7zW89buR4bryY4EDDsCQzsqDJW62GBPSampump</h1>
       <div className="flex-grow md:block hidden" />
       <div className="flex gap-5 justify-center items-center rounded-lg bg-white/30 p-5 py-2 border border-white">
         <a href="https://t.me/chonkesolana" target="_blank" rel="noreferrer">

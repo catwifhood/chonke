333ff6f9c2fa5722a2a2d2aaa9f638876933f69a
catwifhood <165341190+catwifhood@users.noreply.github.com>
2024-06-06 18:53:06 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,17 +6,13 @@ export default function Header() {
       <h1 className="text-2xl font-bold">CA: COMING SOON</h1>
       <div className="flex-grow md:block hidden" />
       <div className="flex gap-5 justify-center items-center rounded-lg bg-white/30 p-5 py-2 border border-white">
-        <a
-          href="https://t.me/chonkesolana"
-          target="_blank"
-          rel="noreferrer"
-        >
+        <a href="https://t.me/chonkesolana" target="_blank" rel="noreferrer">
           <img src="/telegram.png" alt="image" className="w-10 h-10" />
         </a>
         <a href="https://x.com/ChonkeOnSolana" target="_blank" rel="noreferrer">
           <img src="/x.png" alt="image" className="w-10 h-10" />
         </a>
-        <a href="https://pump.fun/" target="_blank" rel="noreferrer">>
+        <a href="https://pump.fun/" target="_blank" rel="noreferrer">
           <img src="/pill.png" alt="image" className="w-10 h-10" />
         </a>
       </div>
